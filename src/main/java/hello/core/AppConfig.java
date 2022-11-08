4c30996066c9de8c93489946547ec5dcb6944755
KRDJK <kdf556@naver.com>
2022-11-08 10:37:11 +0900

    [22.11.08] appConfig라는 공연기획자와 같은 주입용 객체 활용하여 DIP 원칙 준수

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig { // 공연 기획자 역할을 수행한다고 보면 된다. 누굴 캐스팅할지는 얘가 결정!
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+}
