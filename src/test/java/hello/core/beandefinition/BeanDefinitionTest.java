5e9a370076f93f1a718def17d76360fdd548ec6f
KRDJK <kdf556@naver.com>
2022-11-17 16:49:57 +0900

    [22.11.08] 싱글톤 패턴의 문제점 학습 직전 커밋

---
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,28 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class BeanDefinitionTest {
+
+    // 2개 다 정상 작동
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class); // factory method를 통해서 빈을 등록하는 방식
+//    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml"); // 얘는 좀 다름
+
+    @Test
+    @DisplayName("빈 설정 메타정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
