ac6b649245637e47dfdd1c3998055c8852d84d86
KRDJK <kdf556@naver.com>
2022-11-17 17:45:03 +0900

    [22.11.08] @Configuration의 비밀까지 학습 (+ CGLIB)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -11,21 +11,27 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration // 이 어노테이션을 붙이지 않아도 빈 등록은 수행되지만.. 스프링 컨테이너의 싱글톤 관리가 안된다. @CGLIB 기술을 쓰지 못함.
 public class AppConfig { // 공연 기획자 역할을 수행한다고 보면 된다. 누굴 캐스팅할지는 얘가 결정!
+
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
